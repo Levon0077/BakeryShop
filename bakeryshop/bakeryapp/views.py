628f593c95badab8a7c946461fc1ef1d83100671
Levon-0077 <levongrigoryan9555@gmail.com>
2025-01-04 15:48:44 +0100

    defaulttimer

--- bakeryshop/bakeryapp/views.py
+++ bakeryshop/bakeryapp/views.py
@@ -1,6 +1,12 @@
+from timeit import default_timer
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def shop_index(request: HttpRequest) -> HttpResponse:
-    return render(request, 'bakeryapp/index.html')
+
+    context = {
+        'runtime':default_timer()
+    }
+
+    return render(request, 'bakeryapp/index.html', context=context)
